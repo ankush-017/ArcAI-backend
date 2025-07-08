fc067a37a24f3b484b9579f7470aa5324fce7fc3
Ankush kumar <ankushkumar1412005@gmail.com>
2025-07-08 13:32:48 +0530

    add cors

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import authRoutes from './Routes/authRoutes.js'
 
 dotenv.config();
 
-const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'https://arcaiengineer.vercel.app',
@@ -18,13 +17,15 @@ const allowedOrigins = [
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true); // allow curl/Postman
-    if (allowedOrigins.some(allowed => origin.startsWith(allowed))) {
+    if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
     console.log("Blocked by CORS:", origin);
     return callback(new Error('Not allowed by CORS'));
   },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
