ec2025c1d90ba99e4e016cb07a50159f4c341fab
Ankush kumar <ankushkumar1412005@gmail.com>
2025-08-16 23:32:33 +0530

    added upstash

--- server.js
+++ server.js
@@ -3,21 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './Config/db.js';
 import authRoutes from './Routes/authRoutes.js';
-
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173',
-    'https://arcai0.netlify.app',
-    'https://arcaiengineer.vercel.app',
-    'https://arcai.engineer',
-    'https://www.arcai.engineer'
-  ],
+  origin: '*',  // allow all origins for quick test ONLY
   credentials: true,
 }));
-
 app.use(express.json());
 
 connectDB();
@@ -25,7 +18,6 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('server sun raha hai');
 });
-
 app.use('/api/auth', authRoutes);
 app.use('/api/user', authRoutes);
 
